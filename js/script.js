390031b716664a14f1a75aee2b0d08065cdb09bc
kang2oon <kang2oon@gmail.com>
2020-01-22 17:30:57 +0900

    1st commit

---
+++ js/script.js
@@ -0,0 +1,12 @@
+$(document).ready(function(){
+	$('select').niceSelect();
+	$('.date-pick').datepicker({
+		language: 'ko',
+		todayButton: new Date(),
+		clearButton: true
+	});
+	$('input').iCheck({
+		checkboxClass: 'icheckbox_minimal-blue',
+		radioClass: 'iradio_minimal-blue'
+	});
+});
