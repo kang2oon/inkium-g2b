c7dc46fe91b8dff17e8f256dbef4349965d2b0f6
kang2oon <kang2oon@gmail.com>
2020-01-28 16:52:08 +0900

    회원관련 페이지

--- js/script.js
+++ js/script.js
@@ -10,4 +10,13 @@ $(document).ready(function(){
 		radioClass: 'iradio_minimal-blue'
 	});
 	$('.tab').stickyNavbar();
+	$(".tab-content").hide();
+	$(".tab-content:first").show();
+	$("ul.tab-sty3 li").click(function () {
+		$("ul.tab-sty3 li").removeClass("active");
+		$(this).addClass("active");
+		$(".tab-content").hide()
+		var activeTab = $(this).attr("rel");
+		$("#" + activeTab).fadeIn()
+	});
 });
